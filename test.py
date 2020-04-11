58703b53ae80077aec5b1b6bd138842653105964
Oleg <kov2905@icloud.com>
2020-04-11 13:40:34 +0700

    Add hotfix

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 lst = list('Запуск программы')
 print(lst)
+
+# Вставляем элемент 2 на место с индексом 1
+lst = [1, 3, 4]
+lst.insert(1, 2)
+print(lst)
+
+# Добавляем элемент 5 в конец списка
+lst.append(5)
+print(lst)
+
+# Развернем список 
+lst.reverse()
+print(lst)
