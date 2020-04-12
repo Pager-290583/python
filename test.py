c8b020106f4102ce39dde1c5345003937818f791
Oleg <kov2905@icloud.com>
2020-04-12 09:18:27 +0700

    Creat text.txt

--- test.py
+++ test.py
@@ -11,20 +11,4 @@ lst.append(5)
 
 
 print(lst)
-<<<<<<< HEAD
 print(lst1)
-=======
-
-# Вставляем элемент 2 на место с индексом 1
-lst = [1, 3, 4]
-lst.insert(1, 2)
-print(lst)
-
-# Добавляем элемент 5 в конец списка
-lst.append(5)
-print(lst)
-
-# Развернем список 
-lst.reverse()
-print(lst)
->>>>>>> master
